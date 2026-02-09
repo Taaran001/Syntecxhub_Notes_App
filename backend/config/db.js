639d8cdb483f476ba25b1dd6109ba91d70ef6457
Taaran <taaran06@gmail.com>
2026-02-09 17:44:13 +0530

    setup

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+export const connectDB = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB conected!");
+
+    } catch (error) {
+        console.error("Error connecting DB", error);
+        process.exit(1)//1 = exit with failure
+    }
+}
---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import notesRoutes from './routes/notesRoutes.js'
+import { connectDB } from '../config/db.js'
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5001
+
+connectDB()
+
+app.use("/api/notes", notesRoutes)
+
+//Endpoint - combination of URL + HTTP meathod that lets the client interact with a specific resource
+
+app.listen(PORT, ()=>{
+    console.log("server started");
+})
